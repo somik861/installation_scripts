cc7dd2fea98942aad1e3d2e3a823f96d00d7df95
Jan Juračka <xjurack1@fi.muni.cz>
2022-10-03 20:03:29 +0200

    Update utils/init_package_manager.sh

--- utils/init_package_manager.sh
+++ utils/init_package_manager.sh
@@ -5,12 +5,12 @@ source ../common.sh
 if nala --version > /dev/null 2>&1; then
     APT="sudo nala"
     INSTALL="install -y"
-    UPDATE="update -y"
+    UPDATE="update"
     UPGRADE="upgrade -y"
     elif apt --version  > /dev/null 2>&1; then
     APT="sudo apt"
     INSTALL="install -y"
-    UPDATE="update -y"
+    UPDATE="update"
     UPGRADE="upgrade -y"
     elif pacman --version > /dev/null 2>&1; then
     APT="sudo pacman"
