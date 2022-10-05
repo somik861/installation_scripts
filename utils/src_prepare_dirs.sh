c0cefdfc6d39cee84fa6b0c21b36df62243a20e3
Jan <xjurack1@fi.muni.cz>
2022-10-05 10:00:01 +0200

    install updates

--- utils/src_prepare_dirs.sh
+++ utils/src_prepare_dirs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 source src_common.sh
+source util/src_init_shell.sh
 
 __banner Ensuring installation directories
 
@@ -12,3 +13,8 @@ mkdir -p "${SOFTARE_HOME}/lib64"
 mkdir -p "${SOFTARE_HOME}/share/man"
 mkdir -p "${SOFTARE_HOME}/share/info"
 mkdir -p "${SOFTARE_HOME}/include"
+
+_PATH_BIN="export PATH=\"${SOFTARE_HOME}\bin:\$PATH\""
+if ! grep -qx "${_EXPORT_BIN}" "${SHELL_RC}"; then
+    echo "${_PATH_BIN}" >> "${SHELL_RC}"
+fi
