cd5322923731681968b3c65a6ac7fe1851415c16
Jan <xjurack1@fi.muni.cz>
2022-10-06 08:26:14 +0200

    updates

--- packages/build_openmpi.sh
+++ packages/build_openmpi.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
 
 source src_common.sh
-source src_prepare_dirs.sh
+source utils/src_prepare_dirs.sh
+
+trap '__abort' 0
+
+URL="https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.4.tar.bz2"
+FILE="$(basename "$URL")"
+
+
+__wget "$URL" "$FILE"
+tar xf "$FILE"
+
+cd ./openmpi-4.1.4
+./configure --prefix="${SOFTWARE_HOME}"
+make all install
+cd ..
+
+rm -rf "$FILE" openmpi-4.1.4
+
+trap : 0
