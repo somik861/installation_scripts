149dbd4ffc709f431c51560892c822355c1475eb
Jan <xjurack1@fi.muni.cz>
2022-10-06 07:16:50 +0200

    updates

--- neovim/configure.sh
+++ neovim/configure.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+source src_common.sh
+
+trap '__abort' 0
+
 __banner Copying neovim configuration files
 
 rm -rf ~/.config/nvim
 cp -r neovim/config/nvim ~/.config/
+nvim --headless +PlugInstall +qa
+
+trap : 0
