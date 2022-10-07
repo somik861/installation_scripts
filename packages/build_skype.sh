1bd9258ebeab2f4c43f83c61580ff487f3ba45aa
Jan <xjurack1@fi.muni.cz>
2022-10-07 10:54:54 +0200

    install skype

---
+++ packages/build_skype.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+source src_common.sh
+source utils/src_init_package_manager.sh
+
+trap '__abort' 0
+
+URL="https://go.skype.com/skype.download"
+FILE="./skype.deb"
+
+__wget "$URL" "$FILE"
+
+${APT} ${INSTALL} ${FILE}
+
+rm -rf "$FILE"
+
+trap : 0
