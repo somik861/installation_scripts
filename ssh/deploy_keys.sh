12ab555d706921746b13aa7a5b27593768a24109
Jan <xjurack1@fi.muni.cz>
2022-10-07 11:18:04 +0200

    ssh key deploy

--- ssh/deploy_keys.sh
+++ ssh/deploy_keys.sh
@@ -15,7 +15,7 @@ KEY=$(cat "$HOME/.ssh/id_ed25519.pub")
 for HOST in $HOSTS; do
     echo "Proccessing ${HOST}:"
     if ! ssh ${HOST} "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && if ! grep -qx \"${KEY}\" ~/.ssh/authorized_keys; then echo \"${KEY}\" >> ~/.ssh/authorized_keys; fi"; then 
-        echo "${HOST} did not recieve key"
+        echo "### WARNING: ${HOST} did not recieve key"
     fi
 done
 
