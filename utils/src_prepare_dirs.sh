d64350a6cc50bb554673c5b29879c4ce737e8e25
Jan <xjurack1@fi.muni.cz>
2022-10-07 10:35:46 +0200

    finished path exports

--- utils/src_prepare_dirs.sh
+++ utils/src_prepare_dirs.sh
@@ -18,3 +18,30 @@ _PATH_BIN="export PATH=\"${SOFTWARE_HOME}/bin:\$PATH\""
 if ! grep -qx "${_PATH_BIN}" "${SHELL_RC}"; then
     echo "${_PATH_BIN}" >> "${SHELL_RC}"
 fi
+
+_PATH_LDLIB="export LD_LIBRARY_PATH=\"${SOFTWARE_HOME}/lib:${SOFTWARE_HOME}/lib64:\$LD_LIBRARY_PATH\""
+if ! grep -qx "${_PATH_LDLIB}" "${SHELL_RC}"; then
+    echo "${_PATH_LDLIB}" >> "${SHELL_RC}"
+fi
+
+_PATH_MAN="export MANPATH=\"${SOFTWARE_HOME}/share/man:\$MANPATH\""
+if ! grep -qx "${_PATH_MAN}" "${SHELL_RC}"; then
+    echo "${_PATH_MAN}" >> "${SHELL_RC}"
+fi
+
+_PATH_INFO="export INFOPATH=\"${SOFTWARE_HOME}/share/info:\$INFOPATH\""
+if ! grep -qx "${_PATH_INFO}" "${SHELL_RC}"; then
+    echo "${_PATH_INFO}" >> "${SHELL_RC}"
+fi
+
+_PATH_CINCL="export C_INCLUDE_PATH=\"${SOFTWARE_HOME}/include:\$C_INCLUDE_PATH\""
+if ! grep -qx "${_PATH_CINCL}" "${SHELL_RC}"; then
+    echo "${_PATH_CINCL}" >> "${SHELL_RC}"
+fi
+
+_PATH_CPPINCL="export CPLUS_INCLUDE_PATH=\"${SOFTWARE_HOME}/include:\$CPLUS_INCLUDE_PATH\""
+if ! grep -qx "${_PATH_CPPINCL}" "${SHELL_RC}"; then
+    echo "${_PATH_CPPINCL}" >> "${SHELL_RC}"
+fi
+
+
