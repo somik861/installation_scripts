72e22cb5d4637d3e378ed8056c0a6e35f20d3192
Jan Juračka <xjurack1@fi.muni.cz>
2022-10-10 19:35:15 +0200

    Update neovim/configure.sh

--- neovim/configure.sh
+++ neovim/configure.sh
@@ -8,6 +8,9 @@ __banner Copying neovim configuration files
 
 rm -rf ~/.config/nvim
 cp -r neovim/config/nvim ~/.config/
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim --headless +PlugInstall +qa
 
 trap : 0
